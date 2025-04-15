46587302c03d937e2f1147eaea6876a36439a601
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-04-15 17:35:12 +0630

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 MONGO_DB_NAME = "telegram_bot"
 ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
 FORCE_SUB_CHANNELS = [int(x) for x in os.getenv("FORCE_SUB_CHANNELS", "").split(",") if x]
-REFERRAL_THRESHOLD = int(os.getenv("REFERRAL_THRESHOLD", 15))
+REFERRAL_THRESHOLD = int(os.getenv("REFERRAL_THRESHOLD", 1))
 DEFAULT_EARNINGS_MMK = int(os.getenv("DEFAULT_EARNINGS_MMK", 50))
