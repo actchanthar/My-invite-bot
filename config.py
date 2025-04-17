384a973b4b7d38f616825393c3d428b967978e23
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-04-17 11:24:01 +0630

    Add files via upload

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ ADMIN_IDS = [int(admin_id) for admin_id in os.getenv("ADMIN_IDS", "").split(",")
 FORCE_SUB_CHANNELS = os.getenv("FORCE_SUB_CHANNELS", "").split(",")
 
 # Load REFERRAL_THRESHOLD and DEFAULT_EARNINGS_MMK with logging
-REFERRAL_THRESHOLD = int(os.getenv("REFERRAL_THRESHOLD", 15))
-DEFAULT_EARNINGS_MMK = int(os.getenv("DEFAULT_EARNINGS_MMK", 20))
+REFERRAL_THRESHOLD = int(os.getenv("REFERRAL_THRESHOLD", 1))
+DEFAULT_EARNINGS_MMK = int(os.getenv("DEFAULT_EARNINGS_MMK", 50))
 
 logger.info(f"Loaded REFERRAL_THRESHOLD: {REFERRAL_THRESHOLD}")
 logger.info(f"Loaded DEFAULT_EARNINGS_MMK: {DEFAULT_EARNINGS_MMK}")
